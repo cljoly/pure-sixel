9a448e33d277021dda8852f17b37c3c99af16a53
Clément Joly <l@131719.xyz>
2022-08-14 00:22:54 +0100

    docs: add README and license

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pure Rust library to encode images as [Sixel][sixel]
+
+This is currently in development.
+
+## License
+
+The library is covered by the Lesser GPL, version 3 or later. See [COPYING.LESSER][lesser].
+
+[sixel]: https://en.wikipedia.org/wiki/Sixel
+[lesser]: ./COPYING.LESSER
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+/*
+The GPLv3 License (GPLv3)
+
+Copyright (c) 2022 Clément Joly
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #[cfg(test)]
 mod tests {
     #[test]
